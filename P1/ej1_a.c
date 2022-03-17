2ba6f7ee48130c481f9cca836b83c8fb6edaa21b
carlosgruss99@gmail.com <carlosgruss99@gmail.com>
2022-03-17 11:49:30 -0300

    Minor format change

--- P1/ej1_a.c
+++ P1/ej1_a.c
@@ -2,10 +2,12 @@
 
 /* imprime el resultado de OPERANDO1 MOD OPERANDO2 */
 
-#define OPERANDO1 7
+#define OPERANDO1 8
 #define OPERANDO2 3
 
 int main(){
-	printf("El resultado de %d MOD %d es: %d\n", OPERANDO1, OPERANDO2, (OPERANDO1 - (OPERANDO1/OPERANDO2)*OPERANDO2));
+	printf("El resultado de %d MOD %d es: %d\n", OPERANDO1, 
+	OPERANDO2, (OPERANDO1 - (OPERANDO1/OPERANDO2)*OPERANDO2));
+	
 	return 0;
 }
