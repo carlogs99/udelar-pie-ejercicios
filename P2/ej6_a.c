e7143da9530fb45e3d1b7699ac3a19dc516faff1
carlosgruss99@gmail.com <carlosgruss99@gmail.com>
2022-03-24 11:43:20 -0300

    Added ej6

---
+++ P2/ej6_a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/* ojo, no hace ningun chequeo sobre el input */
+
+int main(int argc, char** argv){
+	unsigned int n = atoi(argv[1]);
+	int i = CHAR_BIT*sizeof(UINT_MAX);
+	
+	printf("Representacion binaria: ");
+	while(i--)
+		printf("%d", ((n >> i)&1) );
+	printf("b\n");
+}
