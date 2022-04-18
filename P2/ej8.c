fe21deb528d182974963b2eb2acfcf8a2684f53d
carlosgruss99@gmail.com <carlosgruss99@gmail.com>
2022-04-17 21:10:11 -0300

    added ej8

--- P2/ej8.c
+++ P2/ej8.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 
+#define N 10
+#define M 3
+int numeritos[N] = {-1,1,3,1,2,0,0,1,2,2};
 
-int calcular histograma(int x[], int m, int n, int f[]);
+void calcular_histograma(int x[], int m, int n, int (*f)[M]);
+
+int main(int argc, char** argv){
+	int i;
+	int f[M]; /* frecuencias */
+	float P[M]; /* probabilidades */
+	
+	calcular_histograma(numeritos,M,N,&f);
+	
+	for (i = 0; i < M; ++i) {
+		P[i] = (float)f[i]/(float)N;
+		printf("frecuencia f[%d]=%d -> probabilidad P[%d]=%f\n",i,f[i],i,P[i]);
+	}
+}
+
+void calcular_histograma(int x[], int m, int n, int (*f)[M]){
+	//inicializar el arreglo de frecuencias
+	for(int j = 0 ; j < m ; j++){
+		(*f)[j] = 0;
+	}
+	//crear el histograma clipeando x
+	for(int i = 0 ; i < n ; i++){
+		x[i] = (x[i] < 0) ? 0 : ((x[i] > M - 1) ? (M - 1) : x[i]);
+		(*f)[x[i]]++;
+	}
+}
